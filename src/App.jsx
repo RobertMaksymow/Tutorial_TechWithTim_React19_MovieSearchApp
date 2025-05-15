fd9db0cd218825c785d861588d449e639c72ede5
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-05-15 10:19:41 +0100

    React Router added. Navbar created. Works. Time 0h54min

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div className="App">
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/" className="">
+          Movie App
+        </Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="navbar-link">
+          Home
+        </Link>
+        <Link to="/favorites" className="navbar-link">
+          Favorites
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
